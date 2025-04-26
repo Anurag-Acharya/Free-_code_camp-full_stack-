8022b98dbb3a3d79dc92f625e158668cbc94a012
Anurag Acharya <pas078bei006@wrc.edu.np>
2025-04-26 20:31:22 +0545

    Add files via upload

---
+++ javascript/sentenceMaker.js
@@ -0,0 +1,24 @@
+let adjective,noun,verb,place,adjective2,noun2;
+
+adjective="curious";
+adjective2="big";
+noun="dragon";
+noun2="beast";
+verb="angry";
+place="mountains";
+
+let firstStory="Once upon a time, there was a(n) "+ adjective + " "+ noun +" who loved to eat "+ noun2 +"."+" The "+ noun + " lived in a "+ place+" and had "+adjective2 +" nostrils that blew fire when it was "+ verb +".";
+
+console.log("First story: "+firstStory);
+
+
+adjective="lazy";
+adjective2="small";
+noun="tiger";
+noun2="deer";
+verb="sadden";
+place="cave";
+
+let secondStory="Once upon a time, there was a(n) "+ adjective + " "+noun +" who loved to eat "+ noun2 +"."+" The "+ noun + " lived in a "+ place+" and had "+adjective2 +" nostrils that blew fire when it was "+verb+".";
+
+console.log("Second story: "+secondStory);
